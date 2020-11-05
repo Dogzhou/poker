b5c33f6675cf9365fdbe4555276caa95cfb1d30f
Edward Zhou <Edward_mjz@hotmail.com>
2020-11-05 16:43:23 +1100

    Add straight_flush implementation

--- lib/poker/classify.ex
+++ lib/poker/classify.ex
@@ -3,7 +3,9 @@ defmodule Classify do
   Classify a hand category
   """
   alias Poker.Hand
-  @typep category :: :flush | :four_of_a_kind | :full_house | :high_card | :pair | :straight | :straight_flush | :three_of_a_kind | :two_pairs
+  @typep category :: :flush | :four_of_a_kind | :full_house | :high_card | :pair | :straight | :straight_flush | :three_of_a_kind | :two_pairs | :category
+  @a_high_flush "2345678910JQKA"
+  @a_low_flush  "A2345678910JQK"
 
   @doc """
   Return a category for a hand
@@ -12,9 +14,30 @@ defmodule Classify do
   def classify(hand) do
     cond do
       straight_flush?(hand) -> :straight_flush
+      four_of_a_kind?(hand) -> :four_of_a_kind
       true -> :pair
     end
   end
 
-  defp straight_flush?(hand), do: true
+  defp straight_flush?(hand) do
+    flush?(hand) && straight?(hand)
+  end
+
+  defp flush?(hand) do
+    hand.cards
+    |> Enum.map(fn card -> card.suit end)
+    |> Enum.uniq()
+    |> length()
+    |> Kernel.==(1)
+  end
+
+  defp straight?(hand) do
+    hand_values = Enum.map_join(hand.cards, "", fn card -> card.value end)
+
+    @a_high_flush =~ hand_values || @a_low_flush =~ hand_values
+  end
+
+  defp four_of_a_kind?(hand) do
+    true
+  end
 end
--- test/poker/classify_test.exs
+++ test/poker/classify_test.exs
@@ -7,7 +7,36 @@ defmodule ClassifyTest do
 
     test "without special letters" do
       hand = Hand.init(~w(4D 5D 6D 7D 8D))
+
       assert Classify.classify(hand) == :straight_flush
+    end
+
+    test "with special letters" do
+      hand = Hand.init(~w(10D JD QD KD AD))
+
+      assert Classify.classify(hand) == :straight_flush
+    end
+
+    test "with A as lowest letters" do
+      hand = Hand.init(~w(AD 2D 3D 4D 5D))
+
+      assert Classify.classify(hand) == :straight_flush
+    end
+
+    test "with A as highest letters" do
+      hand = Hand.init(~w(10D JD QD KD AD))
+
+      assert Classify.classify(hand) == :straight_flush
+    end
+  end
+
+  describe "four of a kind" do
+    @describetag :function
+
+    test "returns true" do
+      hand = Hand.init(~w(3D 5D 6D 7D 8D))
+
+      assert Classify.classify(hand) == :four_of_a_kind
     end
   end
 end
