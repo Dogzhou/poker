8c7ddec0f8c57d903ce8e62bda7d235fbf85d43b
Edward Zhou <Edward_mjz@hotmail.com>
2020-11-05 16:28:51 +1100

    Fix Hand

--- lib/poker/hand.ex
+++ lib/poker/hand.ex
@@ -1,25 +1,35 @@
 defmodule Poker.Hand do
   @moduledoc false
   alias Poker.{Card, Hand}
-  @enforce_keys [:cards]
+  @enforce_keys [:cards, :category]
   defstruct [:cards, :category]
 
   @type t() :: %__MODULE__{
     cards: [Card.t()],
-    category: String.t()
+    category: :atom
   }
 
   @doc """
   Init Hand struct
   ## Examples
 
-    iex> alias Poker.{Card, Hand}
-    iex> Hand.init(~w(4D 5S 6S 8D 3C))
-    iex> %Hand{cards: [%Card{suit: "D", value: "4"}, %Card{suit: "S", value: "4"}, %Card{suit: "S", value: "6"}, %Card{suit: "D", value: "8"}, %Card{suit: "C", value: "3"}]}
+    iex> Poker.Hand.init(~w(4D 5S 6S 8D 3C))
+    %Poker.Hand{
+      cards: [
+        %Poker.Card{suit: "D", value: "4"},
+        %Poker.Card{suit: "S", value: "5"},
+        %Poker.Card{suit: "S", value: "6"},
+        %Poker.Card{suit: "D", value: "8"},
+        %Poker.Card{suit: "C", value: "3"}],
+      category: :unknown}
   """
-  @spec init(string :: [String.t()]) :: Hand.t()
-  def init(string) do
-    string
-    |> Enum.map(&(Card.init(&1)))
+  @spec init(string :: [[String.t()]]) :: Hand.t()
+  def init(input) do
+    cards = input |> Enum.map(&(Card.init(&1)))
+
+    %__MODULE__{
+      cards: cards,
+      category: :unknown
+    }
   end
 end
